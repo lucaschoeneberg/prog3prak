bdab1d053425e4e03deb39a504d63efc30b5e44a
Luca Schöneberg <luca-schoeneberg@outlook.com>
2022-11-09 13:00:18 +0100

    add test for Quadrat

--- src/ab02/QuadratTest.java
+++ src/ab02/QuadratTest.java
@@ -1,4 +1,29 @@
 package ab02;
 
+import ab02.ui.Quadrat;
+import ab02.util.Interaktionsbrett;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class QuadratTest {
+    private Interaktionsbrett ib;
+    private Quadrat cube;
+
+    @BeforeEach
+    void setUp() {
+        ib = new Interaktionsbrett();
+        cube = new Quadrat(1, 1, 40);
+    }
+
+    @Test
+    void drawFrame() {
+        System.out.println("Test: draw Frame of Quadrat.");
+        cube.drawFrame(ib);
+    }
+
+    @Test
+    void drawFilling() {
+        System.out.println("Test: draw Filed Frame of Quadrat.");
+        cube.drawFilling(ib);
+    }
 }
