2a98cc72a53e14b6544e9a1acbb7ccd640c924a0
B0my <sergej.spaet@googlemail.com>
2022-11-22 18:00:20 +0100

    s

--- src/ab04/gamelogic/CollisionDetection.java
+++ src/ab04/gamelogic/CollisionDetection.java
@@ -15,25 +15,19 @@ public class CollisionDetection {
     }
 
     public void checkCollisionWithField(Ball ball) {
-        if(ball.getForm().up() <= this.field.getTopY() || ball.getForm().down() >= this.field.getBottomY()) {
+        if (ball.getForm().up() <= this.field.getTopY() || ball.getForm().down() >= this.field.getBottomY()) {
             ball.revertVerticalMovement();
         }
     }
 
     public void checkCollisionWithPaddle(Ball ball) {
-        int randomAngle = (int) (Math.random() * 10);
-
         if (playerLeft.getPaddle().crosses(ball.getForm())) {
             ball.revertHorizontalMovement();
-            if(randomAngle > 5) {
-                ball.revertVerticalMovement();
-            }
+            ball.revertVerticalMovement();
         }
         if (playerRight.getPaddle().crosses(ball.getForm())) {
             ball.revertHorizontalMovement();
-            if(randomAngle > 5) {
-                ball.revertVerticalMovement();
-            }
+            ball.revertVerticalMovement();
         }
     }
 }
