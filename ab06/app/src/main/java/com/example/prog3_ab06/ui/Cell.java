c205977abe4c6d88e3c53c60f329a4fdd64b24c0
Luca Schöneberg <luca-schoeneberg@outlook.com>
2022-12-10 17:26:31 +0100

    Update Cell.java

--- ab06/app/src/main/java/com/example/prog3_ab06/ui/Cell.java
+++ ab06/app/src/main/java/com/example/prog3_ab06/ui/Cell.java
@@ -1,5 +1,10 @@
 package com.example.prog3_ab06.ui;
 
+import android.graphics.Color;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cell {
     private CellType type;
     private CellStatus status = CellStatus.UNREVEALED;
@@ -22,6 +27,10 @@ public class Cell {
         return type == CellType.MINE;
     }
 
+    public boolean isBlanc() {
+        return type == CellType.BLANK;
+    }
+
     public void setMine() {
         this.type = CellType.BLANK;
     }
@@ -30,7 +39,46 @@ public class Cell {
         return status == CellStatus.UNREVEALED;
     }
 
-    public void triggered() {
+    public int getColor() {
+        switch (neighboursMineCount) {
+            case 1: {
+                return Color.BLUE;
+            }
+            case 2: {
+                return Color.GREEN;
+            }
+            case 3: {
+                return Color.RED;
+            }
+            default:{
+                return Color.WHITE;
+            }
+        }
+    }
 
+    public List<Cell> getNeighbours(Cell[][] list, int xPos,int yPos){
+        List<Cell> adjacentCells = new ArrayList<>();
+
+        List<Cell> cellsList = new ArrayList<>();
+        cellsList.add(cellAt(xPos-1, yPos));
+        cellsList.add(cellAt(x+1, y));
+        cellsList.add(cellAt(x-1, y-1));
+        cellsList.add(cellAt(x, y-1));
+        cellsList.add(cellAt(x+1, y-1));
+        cellsList.add(cellAt(x-1, y+1));
+        cellsList.add(cellAt(x, y+1));
+        cellsList.add(cellAt(x+1, y+1));
+
+        for (Cell cell: cellsList) {
+            if (cell != null) {
+                adjacentCells.add(cell);
+            }
+        }
+
+        return adjacentCells;
+    }
+
+    public Integer getNeighboursMineCount() {
+        return neighboursMineCount;
     }
 }
