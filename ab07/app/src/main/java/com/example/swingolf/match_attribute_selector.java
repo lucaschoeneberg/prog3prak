f6db12f30c0a7d61a188dde0cd41856c0aea492d
Sergej <sergej.spaet@hs-osnabrueck.de>
2023-01-03 19:55:05 +0100

    Trauriges xml

---
+++ ab07/app/src/main/java/com/example/swingolf/match_attribute_selector.java
@@ -0,0 +1,14 @@
+package com.example.swingolf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class match_attribute_selector extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_match_attribute_selector);
+    }
+}
